f589e3e02933d9c083b9923267adef31bb5f3a5e
Ian Smith <ian.douglas.smith.1985@gmail.com>
2022-11-06 18:43:40 -0500

    Add date range generator function; add weekly placeholder

--- app/app.py
+++ app/app.py
@@ -5,6 +5,8 @@ import requests
 import json
 
 from calendar import monthrange
+import datetime
+import time
 
 app = FlaskAPI(__name__)
 
@@ -61,6 +63,15 @@ def notes_detail(key):
 def validate_query_params():
     return True
 
+def getDateListFromWeek(year,week):
+    firstdayofweek = datetime.datetime.strptime(f'{year}-W{int(week )- 1}-1', "%Y-W%W-%w").date()
+    datelist = []
+    for i in range(0,7):
+        day = firstdayofweek + datetime.timedelta(days=i)
+        datelist.append(day.split('-'))
+    return datelist
+
+
 @app.route('/pageviews/<int:year>/<int:month>/<int:day>/')
 def example(year,month,day):
     
@@ -113,6 +124,42 @@ def monthly_totals(year,month):
     sorted_top_views = sorted(top_views.items(), key=lambda x: x[1], reverse=True)
 
     return sorted_top_views
+
+@app.route('/pageviews/weekly/<int:year>/<int:week>/')
+def weekly_totals(year,week):
+    
+    data_aggregate = []
+    for i in range(1,monthmax+1):
+        url = f"https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access/{str(year)}/{str(month).zfill(2)}/{str(i).zfill(2)}"
+        print(url)
+        r = requests.get(url, headers=headers)
+        try:
+            data = json.loads(r.text)["items"][0]["articles"]
+        except Exception as e:
+            print(e)
+            return "There was an error processing this request"
+        return data
+        data_aggregate+=data
+
+    top_views = {}
+    for item in data_aggregate:
+        top_views[item["article"]] = top_views.get(item["article"], 0) + item["views"]
+
+    sorted_top_views = sorted(top_views.items(), key=lambda x: x[1], reverse=True)
+
+    return sorted_top_views
+
+
+@app.route('/pageviews/monthly/per_article/<string:article>/<int:year>/<int:month>/')
+def monthly_total_by_article(article, year,month):
+    url =f" https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/all-agents/Albert_Einstein/daily/2015100100/2015103100"
+    # url = f"https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikisource/all-access/{year}/{month}/all-days"
+    r = requests.get(url, headers=headers)
+    try:
+        data = json.loads(r.text)["items"][0]["articles"]
+    except Exception as e:
+        return r.text
+    return data
 
 
 if __name__ == "__main__":
