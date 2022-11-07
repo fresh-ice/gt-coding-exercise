0233b4a05d7d62d9bf867e30a4b0b204b5eda1fa
Ian Smith <ian.douglas.smith.1985@gmail.com>
2022-11-07 13:06:59 -0500

    Clean up

--- app/app.py
+++ app/app.py
@@ -8,21 +8,19 @@ from calendar import monthrange
 import datetime
 import time
 
-
+import markdown
 
 app = FlaskAPI(__name__)
 
 ''' Globals '''
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'} 
+# must include a user agent header because wikimedia blocks requests via script
 
-
-# must include a humanistic user agent header because wikimedia blocks requests via script
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 '''Helper functions'''
-
-
 def validate_query_params():
     return True
+
 
 def getDateListFromWeek(year,week):
     firstdayofweek = datetime.datetime.strptime(f'{year}-W{int(week )- 1}-1', "%Y-W%W-%w").date()
@@ -44,39 +42,40 @@ def getDateListFromMonth(year,month):
     return datelist
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET'])
 def homepage():
     """
     Return readme here.
     """
-    return "You are home."
+    with open("../README.md", "r") as f:
+        html = markdown.markdown(f.read())  
+    return html
 
 
 '''Routes'''
-
-
 @app.route('/pageviews/all-articles/daily/<int:year>/<int:month>/<int:day>/')
 def example(year,month,day):
     url = f"https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access/{year}/{month}/{day}"
     print(url)
-    r = requests.get(url, headers=headers)
+    r = requests.get(url, headers=HEADERS)
 
     data = json.loads(r.text)["items"][0]["articles"]
     print(r)
 
     return data
 
+
 # Uses wiki's own monthly all-days endpoint, which seems to return incorrect results
 @app.route('/pageviews/all-articles/monthly_native/<int:year>/<int:month>/')
 def monthly_native_totals(year,month):
-    
     url = f"https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikisource/all-access/{year}/{month}/all-days"
-    r = requests.get(url, headers=headers)
+    r = requests.get(url, headers=HEADERS)
     try:
         data = json.loads(r.text)["items"][0]["articles"]
     except Exception as e:
         return r.text
     return data
+
 
 # Slow, but returns the accurate monthly total of views. Much too slow for production
 @app.route('/pageviews/all-articles/monthly/<int:year>/<int:month>/')
@@ -85,8 +84,7 @@ def monthly_totals(year,month):
     data_aggregate = []
     for i in range(1,monthmax+1):
         url = f"https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access/{str(year)}/{str(month).zfill(2)}/{str(i).zfill(2)}"
-        print(url)
-        r = requests.get(url, headers=headers)
+        r = requests.get(url, headers=HEADERS)
         try:
             data = json.loads(r.text)["items"][0]["articles"]
         except Exception as e:
@@ -101,6 +99,7 @@ def monthly_totals(year,month):
     sorted_top_views = sorted(top_views.items(), key=lambda x: x[1], reverse=True)
 
     return sorted_top_views
+
 
 @app.route('/pageviews/all-articles/weekly/<int:year>/<int:week>/')
 def weekly_totals(year,week):
@@ -109,8 +108,8 @@ def weekly_totals(year,week):
     data_aggregate = []
     for day in date_list:
         url = f"https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access/{str(day[0])}/{str(day[1]).zfill(2)}/{str(day[2]).zfill(2)}"
-        print(url)
-        r = requests.get(url, headers=headers)
+
+        r = requests.get(url, headers=HEADERS)
         try:
             data = json.loads(r.text)["items"][0]["articles"]
         except Exception as e:
@@ -125,6 +124,7 @@ def weekly_totals(year,week):
     sorted_top_views = sorted(top_views.items(), key=lambda x: x[1], reverse=True)
 
     return sorted_top_views
+
 
 @app.route('/pageviews/by-article/<string:article>/monthly/<int:year>/<int:month>/<string:max_views>')
 @app.route('/pageviews/by-article/<string:article>/monthly/<int:year>/<int:month>/')
@@ -134,19 +134,16 @@ def monthly_total_by_article(article, year, month, max_views=False):
     begin_day = date_list[0]
     end_day = date_list[-1]
     date_range = f"{begin_day[0]+begin_day[1].zfill(2)+begin_day[2].zfill(2)}00/{end_day[0]+end_day[1].zfill(2)+end_day[2].zfill(2)}00"
-    print(date_range)
+
     url =f"https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/all-agents/{article}/daily/{date_range}"
 
-    print(url)
-    r = requests.get(url, headers=headers)
+    r = requests.get(url, headers=HEADERS)
     try:
         data = json.loads(r.text)["items"]
     except Exception as e:
         print("There was an error processing upstream response", e)
         return "There was an error processing this request"
     data_aggregate+=data
-
-    print("dataagrerga", data_aggregate)
 
     monthly_high = [0,0]
     top_views = {}
@@ -156,11 +153,8 @@ def monthly_total_by_article(article, year, month, max_views=False):
         if item["views"] > monthly_high[1]:
             monthly_high = [timestamp[:-2], item["views"]]
 
-    print(top_views)
     sorted_top_views = sorted(top_views.items(), key=lambda x: x[1], reverse=True)
 
-
-    print("max views", max)
     if max_views == 'monthly_high':
         return [monthly_high]
 
@@ -178,7 +172,7 @@ def weekly_total_by_article(article, year, week):
     url =f"https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/all-agents/{article}/daily/{date_range}"
 
     print(url)
-    r = requests.get(url, headers=headers)
+    r = requests.get(url, headers=HEADERS)
     try:
         data = json.loads(r.text)["items"]
     except Exception as e:
@@ -195,7 +189,6 @@ def weekly_total_by_article(article, year, week):
     print(top_views)
     sorted_top_views = sorted(top_views.items(), key=lambda x: x[1], reverse=True)
     return sorted_top_views
-
 
 
 if __name__ == "__main__":
